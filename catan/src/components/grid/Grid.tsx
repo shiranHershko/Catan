a9068754801c1fd9794b4c549cb9ec2df497469f
edShiran <shiran.pink@gmail.com>
2025-12-31 21:21:02 +0200

    organize squares

--- catan/src/components/grid/Grid.tsx
+++ catan/src/components/grid/Grid.tsx
@@ -1,16 +1,52 @@
 import { Square } from "../square";
-
+import "./grid.css";
 const Grid = () => {
   const squares = [
-    // pick in random the squares data
-    { type: "tree", color: "green", number: 8 },
-    { type: "rock", color: "gray", number: 3 },
-    { type: "weat", color: "yellow", number: 5 },
+    [
+      { type: "tree", color: "green", number: 8 },
+      { type: "rock", color: "gray", number: 3 },
+      { type: "wheat", color: "rgb(255, 241, 162)", number: 5 },
+    ],
+    [
+      { type: "brick", color: "red", number: 6 },
+      { type: "brick", color: "red", number: 6 },
+      { type: "brick", color: "red", number: 6 },
+      { type: "sheep", color: "lightgreen", number: 9 },
+    ],
+    [
+      { type: "sheep", color: "lightgreen", number: 9 },
+      { type: "brick", color: "red", number: 6 },
+      { type: "brick", color: "red", number: 6 },
+      { type: "brick", color: "red", number: 6 },
+      { type: "sheep", color: "lightgreen", number: 9 },
+    ],
+    [
+      { type: "brick", color: "red", number: 6 },
+      { type: "brick", color: "red", number: 6 },
+      { type: "brick", color: "red", number: 6 },
+      { type: "sheep", color: "lightgreen", number: 9 },
+    ],
+    [
+      { type: "tree", color: "green", number: 8 },
+      { type: "rock", color: "gray", number: 3 },
+      { type: "wheat", color: "rgb(255, 241, 162)", number: 5 },
+    ],
   ];
+
   return (
-    <div>
-      <Square cubeOption={8} />;
-      <Square cubeOption={8} />;
+    <div className="grid">
+      {squares.map((row, rowIndex) => (
+        <div className="row" key={rowIndex}>
+          {row.map((square, index) => (
+            <Square
+              key={index}
+              cubeOption={square.number}
+              type={square.type}
+              color={square.color}
+            />
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
--- catan/src/components/square/Square.tsx
+++ catan/src/components/square/Square.tsx
@@ -1,7 +1,20 @@
 import "./square.css";
 
-const Square = (props: { cubeOption: number }) => {
-  return <div className="square">{props.cubeOption}</div>;
+const Square = (props: { cubeOption: number; type: string; color: string }) => {
+  return (
+    <div
+      className="square"
+      style={
+        {
+          backgroundColor: props.color,
+          "--square-color": props.color,
+        } as React.CSSProperties
+      }
+    >
+      <span className="square-number">{props.cubeOption}</span>
+      <span className="square-type">{props.type}</span>
+    </div>
+  );
 };
 
 export default Square;
